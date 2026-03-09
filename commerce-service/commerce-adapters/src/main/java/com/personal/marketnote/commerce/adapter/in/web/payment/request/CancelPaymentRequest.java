2fd535542d2eb9cd1c9c2c87944a6a73b40d81e9
hellmir <hellmir610@gmail.com>
2026-03-10 07:46:38 +0900

    [#216][#1085] feat: 부분 결제 취소 시 재고 복구 기능 추가 (#1157)
    - [x] CancelPaymentCommand에 cancelProducts 필드 및 CancelProductItem inner record 추가
    - [x] CancelPaymentRequest에 cancelProducts 필드 및 CancelProductItemRequest inner class 추가
    - [x] PaymentRequestToCommandMapper에 cancelProducts 매핑 로직 추가
    - [x] CancelPaymentService에 restorePartialCancelInventory() 메서드 추가
    - [x] CancelPaymentService에 validateCancelProducts() 검증 메서드 추가 (pricePolicyId 중복, 주문 미존재 상품, 수량 초과, 수량 0이하)
    - [x] InvalidCancelProductException 커스텀 예외 추가

--- commerce-service/commerce-adapters/src/main/java/com/personal/marketnote/commerce/adapter/in/web/payment/request/CancelPaymentRequest.java
+++ commerce-service/commerce-adapters/src/main/java/com/personal/marketnote/commerce/adapter/in/web/payment/request/CancelPaymentRequest.java
@@ -2,8 +2,12 @@ package com.personal.marketnote.commerce.adapter.in.web.payment.request;
 
 import com.personal.marketnote.commerce.port.in.command.payment.CancelPaymentCommand;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class CancelPaymentRequest {
@@ -16,4 +20,22 @@ public class CancelPaymentRequest {
 
     @Schema(name = "cancelReason", description = "취소 사유", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
     private String cancelReason;
+
+    @Schema(name = "cancelProducts", description = "취소 대상 상품 목록 (부분취소 시 재고 복구용, 선택)", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Valid
+    private List<CancelProductItemRequest> cancelProducts;
+
+    @Getter
+    public static class CancelProductItemRequest {
+        @Schema(description = "가격 정책 ID", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotNull(message = "가격 정책 ID는 필수입니다")
+        @Min(1)
+        private Long pricePolicyId;
+
+        @Schema(description = "취소 수량", requiredMode = Schema.RequiredMode.REQUIRED)
+        @NotNull(message = "취소 수량은 필수입니다")
+        @Min(1)
+        private Integer quantity;
+    }
 }
+
---
+++ commerce-service/commerce-application/src/main/java/com/personal/marketnote/commerce/exception/InvalidCancelProductException.java
@@ -0,0 +1,7 @@
+package com.personal.marketnote.commerce.exception;
+
+public class InvalidCancelProductException extends IllegalArgumentException {
+    public InvalidCancelProductException(String message) {
+        super(message);
+    }
+}
