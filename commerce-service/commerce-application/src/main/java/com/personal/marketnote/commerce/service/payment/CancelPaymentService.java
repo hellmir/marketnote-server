3fbacc3641e61a2b6e09d0789eba28763f34ade9
hellmir <hellmir610@gmail.com>
2026-03-29 00:40:16 +0900

    [#929][#1176] feat: 결제 취소 시 리워드 서비스 상품 구매 적립 예정 포인트 회수 연동 기능 Kafka 이벤트 전환 (#1631)
    - [x] PaymentCancelledPendingProductPointConsumer FIXME 제거, CancelPendingPointUseCase 활성화
    - [x] CancelPaymentService에서 revokePendingProductAccumulationPoints HTTP 호출 제거
    - [x] Consumer 테스트 CancelPendingPointUseCase verify 추가
    - [x] CancelPaymentUseCaseTest revokePendingPoints verify 제거

--- commerce-service/commerce-application/src/main/java/com/personal/marketnote/commerce/service/payment/CancelPaymentService.java
+++ commerce-service/commerce-application/src/main/java/com/personal/marketnote/commerce/service/payment/CancelPaymentService.java
@@ -114,7 +114,6 @@ public class CancelPaymentService implements CancelPaymentUseCase {
             );
             restoreInventory(order);
             refundPoints(order);
-            revokePendingProductAccumulationPoints(order);
             revokePendingSharedPurchasePoints(order);
         } else {
             restorePartialCancelInventory(order, command);
@@ -282,15 +281,6 @@ public class CancelPaymentService implements CancelPaymentUseCase {
                         "취소 수량이 주문 수량을 초과합니다. pricePolicyId=" + item.pricePolicyId()
                                 + ", 주문수량=" + orderQuantity + ", 취소수량=" + item.quantity());
             }
-        }
-    }
-
-    private void revokePendingProductAccumulationPoints(Order order) {
-        try {
-            modifyUserPointPort.revokePendingPoints(order.getBuyerId(), order.getId());
-        } catch (Exception e) {
-            log.error("상품 적립 예정 포인트 회수 실패 - orderId: {}, buyerId: {}, error: {}",
-                    order.getId(), order.getBuyerId(), e.getMessage(), e);
         }
     }
 
