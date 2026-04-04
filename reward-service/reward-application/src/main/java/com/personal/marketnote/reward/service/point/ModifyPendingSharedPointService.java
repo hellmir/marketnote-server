e32d10c8625e795fc6f9d64cb64358dd91372663
hellmir <hellmir610@gmail.com>
2026-04-04 13:58:26 +0900

    [#387][#1786] fix: 공유 포인트 적립 시 구매자와 공유자 동일 여부 검증 기능 추가 (#1817)
    ## Reason
    구매자가 자기 자신을 공유자로 지정하여 공유 포인트를 적립받을 수 있다.
    
    ## Action
    - [x] ModifyPendingSharedPointCommand에 buyerId 필드 추가
    - [x] ModifyPendingSharedPointService에서 buyerId == sharerId 동일인 검증 추가 (동일인이면 스킵)
    - [x] OrderPaymentCompletedSharedPointConsumer에서 command에 buyerId 전달
    - [x] PaymentCancelledPartialSharedPointConsumer에서 command에 buyerId 전달

--- reward-service/reward-application/src/main/java/com/personal/marketnote/reward/service/point/ModifyPendingSharedPointService.java
+++ reward-service/reward-application/src/main/java/com/personal/marketnote/reward/service/point/ModifyPendingSharedPointService.java
@@ -1,6 +1,8 @@
 package com.personal.marketnote.reward.service.point;
 
 import com.personal.marketnote.common.application.UseCase;
+import com.personal.marketnote.common.utility.FormatValidator;
+import com.personal.marketnote.reward.domain.point.UserPoint;
 import com.personal.marketnote.reward.port.in.command.point.ModifyPendingPointCommand;
 import com.personal.marketnote.reward.port.in.command.point.ModifyPendingSharedPointCommand;
 import com.personal.marketnote.reward.port.in.result.point.UpdateUserPointResult;
@@ -8,10 +10,14 @@ import com.personal.marketnote.reward.port.in.usecase.point.ModifyPendingPointUs
 import com.personal.marketnote.reward.port.in.usecase.point.ModifyPendingSharedPointUseCase;
 import com.personal.marketnote.reward.port.out.point.FindUserPointPort;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
 
 import static org.springframework.transaction.annotation.Isolation.READ_COMMITTED;
 
+@Slf4j
 @UseCase
 @RequiredArgsConstructor
 @Transactional(isolation = READ_COMMITTED)
@@ -21,12 +27,19 @@ public class ModifyPendingSharedPointService implements ModifyPendingSharedPoint
 
     @Override
     public UpdateUserPointResult modifyPending(ModifyPendingSharedPointCommand command) {
-        Long userId = findUserPointPort.findByUserKey(command.sharerKey().toString())
-                .orElseThrow(() -> new com.personal.marketnote.reward.exception.UserPointNotFoundException(command.sharerKey().toString()))
-                .getUserId();
+        UserPoint sharerPoint = findUserPointPort.findByUserKey(command.sharerKey().toString())
+                .orElseThrow(() -> new com.personal.marketnote.reward.exception.UserPointNotFoundException(command.sharerKey().toString()));
+
+        Long sharerId = sharerPoint.getUserId();
+
+        if (FormatValidator.hasValue(command.buyerId()) && Objects.equals(command.buyerId(), sharerId)) {
+            log.info("구매자와 공유자가 동일인 (적립 예정 포인트 변경 스킵). buyerId={}, sharerKey={}",
+                    command.buyerId(), command.sharerKey());
+            return null;
+        }
 
         ModifyPendingPointCommand delegateCommand = ModifyPendingPointCommand.builder()
-                .userId(userId)
+                .userId(sharerId)
                 .changeType(command.changeType())
                 .amount(command.amount())
                 .sourceType(command.sourceType())
