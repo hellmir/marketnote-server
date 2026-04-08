d020608f2059d97a92d1cfbcf53eec5b1492b3e1
hellmir <hellmir610@gmail.com>
2026-04-09 07:21:26 +0900

    [#1653][#1666] feat: 기프티콘 인기상품 관리 기능 추가 (#2017)
    - [x] GifticonGoods 도메인에 markPopular()/unmarkPopular() 메서드 추가
    - [x] ManageFeaturedGifticonGoodsCommand 생성
    - [x] ManageFeaturedGifticonGoodsUseCase 인터페이스 생성
    - [x] ManageFeaturedGifticonGoodsService 구현
    - [x] EvictGifticonGoodsCachePort 생성 및 PersistenceAdapter 구현
    - [x] ManageFeaturedGifticonGoodsRequest DTO 생성
    - [x] ManageFeaturedGifticonGoodsApiDocs 합성 애노테이션 생성
    - [x] AdminGifticonGoodsController에 PATCH /featured 엔드포인트 추가

--- reward-service/reward-adapters/src/main/java/com/personal/marketnote/reward/adapter/in/web/gifticon/controller/AdminGifticonGoodsController.java
+++ reward-service/reward-adapters/src/main/java/com/personal/marketnote/reward/adapter/in/web/gifticon/controller/AdminGifticonGoodsController.java
@@ -3,17 +3,22 @@ package com.personal.marketnote.reward.adapter.in.web.gifticon.controller;
 import com.personal.marketnote.common.adapter.in.api.format.BaseResponse;
 import com.personal.marketnote.reward.adapter.in.web.gifticon.controller.apidocs.GetAdminGifticonGoodsApiDocs;
 import com.personal.marketnote.reward.adapter.in.web.gifticon.controller.apidocs.ManageGifticonGoodsExposureApiDocs;
+import com.personal.marketnote.reward.adapter.in.web.gifticon.controller.apidocs.ManageFeaturedGifticonGoodsApiDocs;
 import com.personal.marketnote.reward.adapter.in.web.gifticon.controller.apidocs.ManageGifticonGoodsOrderApiDocs;
+import com.personal.marketnote.reward.adapter.in.web.gifticon.request.ManageFeaturedGifticonGoodsRequest;
 import com.personal.marketnote.reward.adapter.in.web.gifticon.request.ManageGifticonGoodsExposureRequest;
 import com.personal.marketnote.reward.adapter.in.web.gifticon.request.ManageGifticonGoodsOrderRequest;
 import com.personal.marketnote.reward.adapter.in.web.gifticon.response.GetAdminGifticonGoodsResponse;
 import com.personal.marketnote.reward.port.in.command.gifticon.GetAdminGifticonGoodsCommand;
+import com.personal.marketnote.reward.port.in.command.gifticon.ManageFeaturedGifticonGoodsCommand;
+import com.personal.marketnote.reward.port.in.command.gifticon.ManageFeaturedGifticonGoodsCommand.FeaturedGoodsItem;
 import com.personal.marketnote.reward.port.in.command.gifticon.ManageGifticonGoodsExposureCommand;
 import com.personal.marketnote.reward.port.in.command.gifticon.ManageGifticonGoodsExposureCommand.ExposureItem;
 import com.personal.marketnote.reward.port.in.command.gifticon.ManageGifticonGoodsOrderCommand;
 import com.personal.marketnote.reward.port.in.command.gifticon.ManageGifticonGoodsOrderCommand.OrderItem;
 import com.personal.marketnote.reward.port.in.result.gifticon.GetAdminGifticonGoodsResult;
 import com.personal.marketnote.reward.port.in.usecase.gifticon.GetAdminGifticonGoodsUseCase;
+import com.personal.marketnote.reward.port.in.usecase.gifticon.ManageFeaturedGifticonGoodsUseCase;
 import com.personal.marketnote.reward.port.in.usecase.gifticon.ManageGifticonGoodsExposureUseCase;
 import com.personal.marketnote.reward.port.in.usecase.gifticon.ManageGifticonGoodsOrderUseCase;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -36,6 +41,7 @@ public class AdminGifticonGoodsController {
     private final GetAdminGifticonGoodsUseCase getAdminGifticonGoodsUseCase;
     private final ManageGifticonGoodsExposureUseCase manageGifticonGoodsExposureUseCase;
     private final ManageGifticonGoodsOrderUseCase manageGifticonGoodsOrderUseCase;
+    private final ManageFeaturedGifticonGoodsUseCase manageFeaturedGifticonGoodsUseCase;
 
     /**
      * (관리자) 기프티콘 전체 상품 목록 조회
@@ -129,6 +135,37 @@ public class AdminGifticonGoodsController {
                         HttpStatus.OK,
                         DEFAULT_SUCCESS_CODE,
                         "기프티콘 상품 노출 순서 관리 성공"
+                )
+        );
+    }
+
+    /**
+     * (관리자) 기프티콘 인기상품 관리
+     *
+     * @param request 인기상품 관리 요청
+     * @Author 성효빈
+     * @Date 2026-04-06
+     * @Description 관리자가 기프티콘 상품의 인기상품 여부와 정렬 순서를 관리합니다.
+     */
+    @PatchMapping("/featured")
+    @PreAuthorize(ADMIN_POINTCUT)
+    @ManageFeaturedGifticonGoodsApiDocs
+    public ResponseEntity<BaseResponse<Void>> manageFeaturedGifticonGoods(
+            @Valid @RequestBody ManageFeaturedGifticonGoodsRequest request
+    ) {
+        ManageFeaturedGifticonGoodsCommand command = new ManageFeaturedGifticonGoodsCommand(
+                request.items().stream()
+                        .map(item -> new FeaturedGoodsItem(item.goodsCode(), item.popular(), item.popularOrderNum()))
+                        .toList()
+        );
+
+        manageFeaturedGifticonGoodsUseCase.manageFeatured(command);
+
+        return ResponseEntity.ok(
+                BaseResponse.of(
+                        HttpStatus.OK,
+                        DEFAULT_SUCCESS_CODE,
+                        "기프티콘 인기상품 관리 성공"
                 )
         );
     }
