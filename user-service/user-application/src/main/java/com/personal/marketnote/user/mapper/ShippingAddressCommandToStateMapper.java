de3b18a932e4ef54807ba6461936068bf85d3149
hellmir <hellmir610@gmail.com>
2026-04-17 07:45:42 +0900

    [#43][#1596] refactor: 회원 서비스 State 생성 로직 CommandToStateMapper 분리 (#2163)
    - [x] RegisterShippingAddressService → ShippingAddressCommandToStateMapper 생성 (ShippingAddressCreateState)

---
+++ user-service/user-application/src/main/java/com/personal/marketnote/user/mapper/ShippingAddressCommandToStateMapper.java
@@ -0,0 +1,33 @@
+package com.personal.marketnote.user.mapper;
+
+import com.personal.marketnote.common.domain.delivery.DeliveryRequestType;
+import com.personal.marketnote.common.utility.FormatValidator;
+import com.personal.marketnote.user.domain.shippingaddress.ShippingAddressCreateState;
+import com.personal.marketnote.user.port.in.command.shippingaddress.RegisterShippingAddressCommand;
+
+public class ShippingAddressCommandToStateMapper {
+    private ShippingAddressCommandToStateMapper() {
+    }
+
+    public static ShippingAddressCreateState mapToCreateState(
+            RegisterShippingAddressCommand command, boolean isDefault
+    ) {
+        return ShippingAddressCreateState.builder()
+                .userId(command.userId())
+                .addressType(command.addressType())
+                .address(command.address())
+                .addressDetail(command.addressDetail())
+                .companyName(command.companyName())
+                .addressAlias(command.addressAlias())
+                .recipientName(command.recipientName())
+                .recipientPhoneNumber(command.recipientPhoneNumber())
+                .deliveryRequestType(
+                        FormatValidator.hasValue(command.deliveryRequestType())
+                                ? command.deliveryRequestType()
+                                : DeliveryRequestType.NONE
+                )
+                .deliveryRequestMessage(command.deliveryRequestMessage())
+                .isDefault(isDefault)
+                .build();
+    }
+}
