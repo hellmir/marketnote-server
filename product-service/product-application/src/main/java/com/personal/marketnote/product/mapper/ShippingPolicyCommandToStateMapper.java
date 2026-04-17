d76b40739b76eba35fefbddedc359bad5c4dcb97
hellmir <hellmir610@gmail.com>
2026-04-17 19:24:25 +0900

    [#128][#1597] refactor: 상품 서비스 State 생성 로직 CommandToStateMapper 분리 (#2165)
    - [x] RegisterShippingPolicyService → ShippingPolicyCommandToStateMapper 생성 (ShippingPolicyCreateState)

---
+++ product-service/product-application/src/main/java/com/personal/marketnote/product/mapper/ShippingPolicyCommandToStateMapper.java
@@ -0,0 +1,19 @@
+package com.personal.marketnote.product.mapper;
+
+import com.personal.marketnote.product.domain.shipping.ShippingPolicyCreateState;
+import com.personal.marketnote.product.port.in.command.RegisterShippingPolicyCommand;
+
+public class ShippingPolicyCommandToStateMapper {
+
+    private ShippingPolicyCommandToStateMapper() {
+    }
+
+    public static ShippingPolicyCreateState mapToState(Long sellerId, RegisterShippingPolicyCommand command) {
+        return ShippingPolicyCreateState.builder()
+                .sellerId(sellerId)
+                .deliveryCompany(command.deliveryCompany())
+                .shippingFee(command.shippingFee())
+                .freeShippingThreshold(command.freeShippingThreshold())
+                .build();
+    }
+}
