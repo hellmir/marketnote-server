cf253536c10a89189ff3cc6e79e57d3bd58702fa
hellmir <hellmir610@gmail.com>
2026-04-23 23:12:02 +0900

    [#300][#2306] refactor: PostSortProperty switch 중복 제거 리팩토링 (#2307)
    - [x] PostSortProperty enum에 sortField 필드 추가
    - [x] GetPostService.getSortField() 메서드 제거 및 sortProperty.getSortField() 대체
    - [x] PostPersistenceAdapter.getSortField() 메서드 제거 및 sortProperty.getSortField() 대체

--- community-service/community-domain/src/main/java/com/personal/marketnote/community/domain/post/PostSortProperty.java
+++ community-service/community-domain/src/main/java/com/personal/marketnote/community/domain/post/PostSortProperty.java
@@ -5,15 +5,17 @@ import lombok.Getter;
 
 @Getter
 public enum PostSortProperty {
-    ORDER_NUM("지정된 정렬 순서순"),
-    ID("기본키(최신순)"),
-    IS_ANSWERED("답변 여부");
+    ORDER_NUM("지정된 정렬 순서순", "orderNum"),
+    ID("기본키(최신순)", "id"),
+    IS_ANSWERED("답변 여부", "id");
 
     private final String description;
     private final String camelCaseValue;
+    private final String sortField;
 
-    PostSortProperty(String description) {
+    PostSortProperty(String description, String sortField) {
         this.description = description;
         this.camelCaseValue = FormatConverter.snakeToCamel(this.name());
+        this.sortField = sortField;
     }
 }
