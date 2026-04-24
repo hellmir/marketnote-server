f431977d9a3baee952cc6cef7a9985c4836a6a0c
hellmir <hellmir610@gmail.com>
2026-04-24 21:54:17 +0900

    [#216][#1932] feat: 피킹 완료 이후 취소 불가 예외 추가 (#2314)
    - [x] OrderCancellationNotAllowedException extends IllegalStateException 변경 (HTTP 409 반환)
    - [x] 에러 메시지 "피킹완료 이후에는 주문 취소가 불가능합니다. 반품으로 처리해 주세요."로 통일
    - [x] CancelOrderService throw 지점 2곳 생성자 단순화

--- commerce-service/commerce-application/src/main/java/com/personal/marketnote/commerce/exception/OrderCancellationNotAllowedException.java
+++ commerce-service/commerce-application/src/main/java/com/personal/marketnote/commerce/exception/OrderCancellationNotAllowedException.java
@@ -1,7 +1,7 @@
 package com.personal.marketnote.commerce.exception;
 
-public class OrderCancellationNotAllowedException extends RuntimeException {
-    public OrderCancellationNotAllowedException(Long orderId, String message) {
-        super("ERR_ORDER_CANCEL_01::주문 취소가 불가합니다. orderId=" + orderId + ", reason=" + message);
+public class OrderCancellationNotAllowedException extends IllegalStateException {
+    public OrderCancellationNotAllowedException(Long orderId) {
+        super("ERR_ORDER_CANCEL_01::피킹완료 이후에는 주문 취소가 불가능합니다. 반품으로 처리해 주세요. orderId=" + orderId);
     }
 }
