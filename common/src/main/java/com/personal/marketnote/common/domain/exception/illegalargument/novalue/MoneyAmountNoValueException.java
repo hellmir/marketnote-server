43e1171dc498e1be09143ef73235caf69c878d15
hellmir <hellmir610@gmail.com>
2026-09-19 15:21:10 +0900

    [#3082][#3083] feat: Money VO 자체 검증 로직 도입
    - [x] Money VO 클래스 생성 (common/domain/money/)
    - [x] of(long), of(Long), zero() 팩토리 메서드 + >= 0 검증
    - [x] add(), subtract(), multiply() 산술 연산 (Math.addExact/subtractExact/multiplyExact)
    - [x] isZero(), isPositive(), isGreaterThan(), isLessThan(), isGreaterThanOrEqual() 비교 메서드
    - [x] getValue() 접근자 + @JsonValue + @JsonCreator
    - [x] equals/hashCode/toString
    - [x] InvalidMoneyAmountException, MoneyAmountNoValueException 커스텀 예외 생성

---
+++ common/src/main/java/com/personal/marketnote/common/domain/exception/illegalargument/novalue/MoneyAmountNoValueException.java
@@ -0,0 +1,7 @@
+package com.personal.marketnote.common.domain.exception.illegalargument.novalue;
+
+public class MoneyAmountNoValueException extends NoValueException {
+    public MoneyAmountNoValueException(String message) {
+        super(message);
+    }
+}
