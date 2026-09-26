94a810265c853332dc685530cd2f70597acf4aff
hellmir <hellmir610@gmail.com>
2026-09-27 07:33:10 +0900

    [#3082][#3100] feat: RecipientName VO 자체 검증 로직 도입 (#3319)
    - [x] RecipientName VO 클래스 생성 (common/domain/recipientname)
    - [x] of(String) 팩토리 메서드 + non-blank + RECIPIENT_NAME_PATTERN 검증
    - [x] getValue() 접근자 (@JsonCreator/@JsonValue)
    - [x] RecipientNameNoValueException / InvalidRecipientNameException 추가

---
+++ common/src/main/java/com/personal/marketnote/common/domain/exception/illegalargument/invalidvalue/InvalidRecipientNameException.java
@@ -0,0 +1,7 @@
+package com.personal.marketnote.common.domain.exception.illegalargument.invalidvalue;
+
+public class InvalidRecipientNameException extends InvalidValueException {
+    public InvalidRecipientNameException(String message) {
+        super(message);
+    }
+}
