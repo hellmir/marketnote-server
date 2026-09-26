b518a4193d850d1dad7b5bef058f137b70bd7027
hellmir <hellmir610@gmail.com>
2026-09-27 06:50:10 +0900

    [#3082][#3104] feat: DeliveryRequestMessage VO 자체 검증 로직 도입
    - [x] DeliveryRequestMessage VO 클래스 생성 (common/domain/deliveryrequestmessage/)
    - [x] of(String) 팩토리 메서드 + non-blank + 60자 이하 검증
    - [x] getValue() 접근자 + @JsonValue + @JsonCreator
    - [x] equals/hashCode/toString
    - [x] InvalidDeliveryRequestMessageLengthException common 생성 (InvalidValueException 상속)
    - [x] DeliveryRequestMessageNoValueException common 생성 (NoValueException 상속)

---
+++ common/src/main/java/com/personal/marketnote/common/domain/exception/illegalargument/invalidvalue/InvalidDeliveryRequestMessageLengthException.java
@@ -0,0 +1,7 @@
+package com.personal.marketnote.common.domain.exception.illegalargument.invalidvalue;
+
+public class InvalidDeliveryRequestMessageLengthException extends InvalidValueException {
+    public InvalidDeliveryRequestMessageLengthException(String message) {
+        super(message);
+    }
+}
